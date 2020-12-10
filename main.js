e2690c5e658502d6330c2ea854f4692799aff648
Alfred <akoo1@cougarmail.collin.edu>
2020-12-10 01:40:23 -0600

    Redesigned banner page, edited self intro, added active nav links, optimized skills cards border shadows

--- main.js
+++ main.js
@@ -7,23 +7,36 @@ else {
 }
 
 function ready() {
-    // -- Responsive nav bar --
-    document.getElementsByClassName('nav-bar__link--hamburger')[0].addEventListener('click', hamburger_clicked)
-    // Another way to select elements
-    // document.querySelector('.hamburger').addEventListener('click', hamburger_clicked)
+    // toggle 'responsive' class to nav-bar when the hamburger is clicked
+    document.querySelector('.nav-bar__link--hamburger').addEventListener('click', () => {
+        let navBar = document.querySelector('.nav-bar')
+        navBar.classList.toggle('responsive');
+    })
 
-    function hamburger_clicked() {
-        var x = document.getElementById('my-nav-bar')
-
-        if (!x.classList.contains('responsive')) {
-            x.classList.add('responsive')
-        } else {
-            x.classList.remove('responsive')
-        }
-    }
-    // -- Add class name 'sticky' to nav bar when scroll down --
+    // toggle 'sticky' class to nav-bar when users scroll down 
     window.addEventListener('scroll', () => {
         document.querySelector('.nav-bar').classList.toggle('sticky', window.scrollY > 0)
     })
+
+
+    // toggle 'current' class to nav links when users scroll to the associated section
+    let mainNavLinks = document.querySelectorAll("nav a:not(:first-child)"); // to exclude the logo nav link
+
+    window.addEventListener("scroll", () => {
+        let fromTop = window.scrollY;
+
+        mainNavLinks.forEach(link => {
+            let section = document.querySelector(link.hash);
+
+            if (
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
+            ) {
+                link.classList.add("current");
+            } else {
+                link.classList.remove("current");
+            }
+        });
+    });
 
 }
